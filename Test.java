cfb1c134cba297b793503416df3723c03e3c7df7
AmulyaAppana <AmulyaAppana@.github.com>
2019-07-18 17:35:13 +0530

    Test java file Modified

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
+import java.util.*;
 class Test{
-public static void main(String [] args){
-System.out.println("Hello My name is amulya");
+	public static void main(String [] args){
+	 Scanner sc= new Scanner(System.in);
+	 String yn=sc.next();
+	 if(yn.equalsIgnoreCase("yes")){
+		System.out.println("Hello My name is amulya");
+	}
+	else{
+		System.out.println("Sorry");
+	}
 }
 }
